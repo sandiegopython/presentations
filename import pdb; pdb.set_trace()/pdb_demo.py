0e2864f6a986af9380abd1fb2e1fc3425d402d79
Trey Hunner <treyhunner@gmail.com>
2012-11-27 20:56:06 -0800

    Add demo for import pdb ; pdb.set_trace() talk

---
+++ import pdb; pdb.set_trace()/pdb_demo.py
@@ -0,0 +1,36 @@
+def read(s):
+    """Do nothing anything at all"""
+
+
+def write(s):
+    """Print out the given string"""
+    print s
+
+
+def some_function(s):  # Type "n" to get to the next line
+    read('Continue execution until function return with "r"')  # Type "r"
+    write("This line skipped")  # Type "r" if you haven't already
+    write("Another skipped line")
+    read('last line')  # Type "n" or "r" again to walk just outside of function
+
+
+def pdb_test():
+    import pdb ; pdb.set_trace()
+    read("continue until next line")  # Type "n" to execute next line
+    read('list surrounding lines with "l"')  # Type "l" and then type "n" again
+    some_function('step into functions by typing "s"')  # Type "s" now
+    read("We are outside of the function now!")  # Type "n"
+    for i in range(1, 3):  # Type "n" again
+        write('You will need to type "n" multiple times for this loop: %s' % i)
+    write('continue execution by typing "c"')  # Type "c" now
+    write('This line and the next one will be skipped since you typed "c"')
+    write("Another skipped line")
+    write('Done.\n')
+    write("more pdb information: http://docs.python.org/2/library/pdb.html\n")
+
+print "import pdb ; pdb.set_trace()"
+print "==\n"
+print "You are about to begin a journey into pdb\n"
+print "Follow instructions on selected lines (lines starting with ->)"
+print "The first one is selected below.  Please read and begin.\n"
+pdb_test()
